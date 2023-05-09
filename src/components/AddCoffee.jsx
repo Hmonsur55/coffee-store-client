ea3872550c935b5e707387890e648ae03aa1951e
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-09 11:00:48 +0600

    add input field for getting data

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,9 +1,37 @@
 
 const AddCoffee = () => {
     return (
-        <div>
-            <h1>add coffee</h1>
-        </div>
+      <div>
+        <h1>add coffee</h1>
+        <form>
+          <div className="flex gap-3 w-4/6 mx-auto">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">coffee name</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  placeholder="coffee name"
+                  className="input input-bordered"
+                />
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </label>
+            </div>
+          </div>
+        </form>
+      </div>
     );
 };
 
