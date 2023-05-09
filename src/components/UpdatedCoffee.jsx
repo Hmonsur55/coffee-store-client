946c1684aa856329ba15cc39244b4bda250b0681
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-09 10:35:31 +0600

    connected mongodb server into client

---
+++ src/components/UpdatedCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdatedCoffee = () => {
+    return (
+        <div>
+            <h2>updated coffee</h2>
+        </div>
+    );
+};
+
+export default UpdatedCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from './App';
+import AddCoffee from './components/AddCoffee';
+import UpdatedCoffee from './components/UpdatedCoffee';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App></App>
+  },
+  {
+    path: '/addcoffee',
+    element: <AddCoffee></AddCoffee>
+  }, {
+    path: '/updatecoffee',
+    element:<UpdatedCoffee></UpdatedCoffee>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
